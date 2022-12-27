abc24afe0d3667d9b336672744d7ba94356431c6
2191706683 <2191706683@qq.com>
2022-12-28 01:51:07 +0800

    今日头条

--- weapp/jrtt/components/explore-content/explore-content.js
+++ weapp/jrtt/components/explore-content/explore-content.js
@@ -11,8 +11,17 @@ Component({
    * 组件的初始数据
    */
   data: {
-    cardList:[
-      {id:1,pic:"https://avatars.githubusercontent.com/u/101383800?s=48&v=4",author:"娱评人糖微甜",date:"9小时前"}
+    cardList: [
+      {
+        id: 1, pic: "https://avatars.githubusercontent.com/u/101383800?s=48&v=4", author: "娱评人糖微甜", date: "9小时前",
+        article: "“京城四少”：有人破产，有人入狱，有人为爱隐退，有人一地鸡毛11年前，网友们“选出”了全是富二代的“京城四少”据说这个名字是根据台湾热播的电视剧《京城四少》得来的。而他们四个人也实至名归：汪雨是上海地产界大亨、汪道涵的三儿子；王珂成立了富承维京投资公司媒体称他身家超200亿；汪小菲曾是俏江南的CEO;王烁是澳籍华人富商王志才之子，王艳是他的继母。“京城四少”，三个是超级富二代，一个是富一代。按理来说，他们的生活应该比每一个人都要好，看他们的前半生，确实也很精彩，流连在各个全文",
+        img: "https://thumbnail1.baidupcs.com/thumbnail/b037591b7h438f60bd04f57c658fbe56?fid=226729182-250528-603507051465878&rt=pr&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-ECz8gSy8BA350zS296m4uRBo3no%3d&expires=8h&chkbd=0&chkv=0&dp-logid=302914531851833033&dp-callid=0&time=1672156800&size=c1536_u864&quality=90&vuk=226729182&ft=image&autopolicy=1"
+      },
+      {
+        id: 2, pic: "https://i0.hdslb.com/bfs/face/143c02f3f7262b7a39952489c981f5fca2e6fb20.jpg@96w_96h_1c_1s.webp", author: "娱评人糖微甜", date: "9小时前",
+        article: "11月28日，广东河源，一女子满着丈夫偷偷去做胸部整形手术的视频引发网友热议。视频显示，男子突然找不到妻子了，他们已经结婚7年，孩子也已经5岁了，但不知妻子为何突然消失了。女子是一名95后，丈夫最后通过各种途.全文",
+        img: "https://thumbnail1.baidupcs.com/thumbnail/9d1a52ea0o2ef8718a561fd2c1826d18?fid=226729182-250528-143625881060147&rt=pr&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-v2dBsmB2B2s7BYQURfXmEkd3ago%3d&expires=8h&chkbd=0&chkv=0&dp-logid=303216431381559523&dp-callid=0&time=1672160400&size=c1536_u864&quality=90&vuk=226729182&ft=image&autopolicy=1"
+      }
     ]
   },
 
