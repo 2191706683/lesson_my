2c42f7b9f4b806895b3d693e422720f03da4300a
2191706683 <2191706683@qq.com>
2023-01-07 15:21:58 +0800

    仿今日头条

--- weapp/jrtt/pages/index/index.js
+++ weapp/jrtt/pages/index/index.js
@@ -1,26 +1,31 @@
 // index.js
 // 获取应用实例
 const app = getApp()
-
 Page({
   data: {
     active: 1,
-    exploreTop:[]
-  
   },
   // 事件处理函数
   onLoad(options) {
-    // const exploreTop = app.globalData.exploreTop
-    // const recommendTop = app.globalData.recommendTop
-    // const exploreList = app.globalData.exploreList
-    // const recommendList = app.globalData.recommendList
-    // const hot = app.globalData.hot
-    this.setData({
-      // exploreTop: app.globalData.exploreTop,
-    //   recommendTop: app.globalData.recommendTop,
-    //   exploreList: app.globalData.exploreList,
-    //   recommendList: app.globalData.recommendList,
-    //   hot: app.globalData.hot
+    wx.request({
+      url: 'https://www.fastmock.site/mock/86972139cfd668eaea8d8f030f197ea0/jrtt/index',
+      success: (res) => {
+        const { 
+          exploreTop, 
+          recommendTop, 
+          exploreList, 
+          recommendList, 
+          hot
+        } = res.data
+        this.setData({
+          exploreTop,
+          recommendTop,
+          exploreList,
+          recommendList,
+          hot
+        })
+      }
     })
+    
   },
 })
