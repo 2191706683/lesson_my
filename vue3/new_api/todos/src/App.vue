80b3bfb857e799586476e3da6049ae27d7f2b074
2191706683 <2191706683@qq.com>
2023-01-10 14:13:43 +0800

    new_api

---
+++ vue3/new_api/todos/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 @click="add" class="red">{{ count }} --- {{ state.count }}</h1>
+  </div>
+</template>
+
+<script setup>
+  import { reactive, ref } from 'vue'
+  // ref 项目式  简单值
+  let state = reactive({
+    count: 1
+  })
+  let count = ref(1)
+  function add() {
+    count.value++
+    state.count++
+  }
+</script>
+
+<style>
+  .red {
+    color: red;
+  }
+</style>
