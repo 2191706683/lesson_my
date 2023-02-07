e09352ce9e17ff9d246f56a219bacb37001ed777
2191706683 <2191706683@qq.com>
2023-02-07 21:26:19 +0800

    vue23

---
+++ vue3/vue23/src/components/Post.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card mt-1">
+    <div class="spinner-border p-2 m-2" role="status" v-if="loading">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <div class="card-body text-start" v-if="!loading">
+      <h5 class="card-title">
+        <a :href="detail.url">{{ detail.title }}</a>
+      </h5>
+      <p class="card-text text-end">
+        Score: <b>{{ detail.score }}</b>
+        <br>
+        by: <i>{{ detail.by }}</i>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted,ref, reactive, defineProps } from 'vue'
+let loading = ref(true)
+const props = defineProps({
+  detail: Object
+})
+const detail = reactive(props.detail)
+onMounted(() => {
+  setTimeout(() => {
+    loading = !loading
+  }, 2000)
+})
+</script>
+
+<style>
+
+</style>
---
+++ vue3/vue23/src/components/Post2.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card mt-1">
+    <div class="spinner-border p-2 m-2" role="status" v-if="loading">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <div class="card-body text-start" v-if="!loading">
+      <h5 class="card-title">
+        <a :href="detail.url">{{ detail.title }}</a>
+      </h5>
+      <p class="card-text text-end">
+        Score: <b>{{ detail.score }}</b>
+        <br>
+        by: <i>{{ detail.by }}</i>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "post",
+    data() {
+      return {
+        loading: true
+      }
+    },
+    props: ["detail"],
+    mounted() {
+      setTimeout(() => {
+        this.loading = !this.loading
+      }, 2000)
+    }
+  }
+</script>
+
+<style>
+
+</style>
