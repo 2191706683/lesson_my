83e0af67e5485db3df77a82d8d00f5289bcc8ce5
2191706683 <2191706683@qq.com>
2023-02-08 16:33:11 +0800

    data-vision

---
+++ vue3/data-vision/zxt/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-button>Hellow World 里的按钮</el-button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue3/data-vision/zxt/src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+// js 使用import 引入css
+// link 传统
+// 更加好统一管理
+import 'element-plus/dist/index.css'
+// 全局引入
+// 性能问题
+import { 
+    // 按需加载
+    ElCard, /* 卡片组件 */
+    ElButton /* 按钮组件 */
+} from 'element-plus' // 亚洲啤酒厂
+
+const app = createApp(App)
+// ElButton 全局组件插入
+app
+    .use(ElButton) // 组件变成全局组件
+    .use(ElCard)
+app.mount('#app')
