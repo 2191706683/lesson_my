30e571169ea991712e7704afc6b6adb4065bfab6
2191706683 <2191706683@qq.com>
2023-03-07 21:25:36 +0800

    hand-vue3-project

---
+++ workflow/hand-vue3-project/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        App
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ workflow/hand-vue3-project/src/main.js
@@ -0,0 +1,10 @@
+// console.log('hello world')
+// import App from './App.vue' // 引入App.vue 但是.vue文件无法识别
+import { createApp } from "vue"; // 依赖关系
+import App from './App.vue' 
+
+const app = createApp(App)
+app.mount('#root')
+
+// const root = document.getElementById('root')
+// root.textContent = '你喵贵姓'
