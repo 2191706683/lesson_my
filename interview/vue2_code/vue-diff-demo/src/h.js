6125302ae23c37f291a2c3c8ed97eeac900f20e9
2191706683 <2191706683@qq.com>
2023-04-19 12:14:33 +0800

    vue-diff-demo

--- interview/vue2_code/vue-diff-demo/src/h.js
+++ interview/vue2_code/vue-diff-demo/src/h.js
@@ -51,6 +51,7 @@ export function h(tag, data=null, children=null) {
         tag,
         flags,
         data,
+        key: data && data.key ? data.key : null,
         children,
         childFlags
     }
--- interview/vue2_code/vue-diff-demo/src/index.js
+++ interview/vue2_code/vue-diff-demo/src/index.js
@@ -1,28 +1,40 @@
 import { h } from './h.js'; // VNODE 的生成
 import render from './render.js' // 渲染
 
-// const handler = () => alert('clicked');
-/* const  prevNode = h('div', {
-    style: {
-        width: '100px',
-        height: '100px',
-        backgroundColor: 'red'
-    },
-    onclick: handler
-}) */
-const prevNode = h(
-    'div',
-    null,
-    h('p', null, '子节点1')
-)
-const nextVNode = h(
-    'div',
-    null,
-    [
-        h('p', null, '子节点1'),
-        h('p', null, '子节点2')
-    ]
-)
+const prevVNode = h('ul', null, [
+    h('li', { key: 'a' }, 1),
+    h('li', { key: 'b' }, 2),
+    h('li', { key: 'c' }, 3)
+  ])
+  
+  // 新的 VNode
+  const nextVNode = h('ul', null, [
+    h('li', { key: 'c' }, 3),
+    h('li', { key: 'a' }, 1),
+    h('li', { key: 'b' }, 2)
+  ])
+
+
+
+
+// const prevNode = h(
+//     'div',
+//     null,
+//     [
+//         h('p', { key: 'a' }, '子节点1'),
+//         h('p', { key: 'b' }, '子节点2'),
+//         h('p', { key: 'c' }, '子节点3')
+//     ]
+// )
+// const nextVNode = h(
+//     'div',
+//     null,
+//     [
+//         h('p', { key: 'd' }, '子节点1'),
+//         h('p', { key: 'a' }, '子节点2'),
+//         h('p', { key: 'b' }, '子节点3')
+//     ]
+// )
 
 /* const nextVNode = h('div', {
     style: {
@@ -33,7 +45,7 @@ const nextVNode = h(
 })
  */
 // const prevNode = h('div', null, '旧的VNode')
-render(prevNode, document.getElementById('app'))
+render(prevVNode, document.getElementById('app'))
 // class MyComponent {
 //     render() {
 //         return h('div', null, '新的VNode')
