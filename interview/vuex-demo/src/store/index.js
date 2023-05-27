327ea49273de2306ec84383e1366bec2c19e13fa
2191706683 <2191706683@qq.com>
2023-05-27 16:45:42 +0800

    vuex-demo

---
+++ interview/vuex-demo/src/store/index.js
@@ -0,0 +1,16 @@
+// 根上的状态
+// 全部放在 state 上
+const state = {
+    // LBS html5 新功能 隐私
+    latitude: '',
+    longitude: '',
+    cartList: {
+
+    },
+    shopDetail: {},
+    userInfo: null,
+    search: '',
+    login: false,
+    address: [],
+    orderList: {}
+}
