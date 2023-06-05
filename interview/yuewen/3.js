e60ff1b492fcc18ad84660aebb6f6bb2342ea0a1
2191706683 <2191706683@qq.com>
2023-06-05 15:34:15 +0800

    阅文面试题

--- interview/yuewen/3.js
+++ interview/yuewen/3.js
@@ -5,4 +5,26 @@ const pathList = [
     '/root/dir2/file3.txt',
     '/root/dir2/subdir2/file4.txt',
     '/root/dir2/subdir3/file5.txt'
-]
+];
+
+// json 树
+const tree = {};
+
+for (let path of pathList) {
+    const parts = path.split('/').filter(part => part !== '');
+    console.log(parts, '////');
+    let currentNode = tree;
+    for (let i = 0; i < parts.legnth - 1; i++) {
+        const part = parts[i];
+        if (!currentNode[part]) {
+            currentNode[part] = {}
+        }
+        currentNode = currentNode[part];
+    }
+    const fileName = parts[parts.length - 1];
+    if (!currentNode.files) {
+        currentNode.files = {};
+    }
+    currentNode.files.push(fileName);
+}
+console.log(tree);
